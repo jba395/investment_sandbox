5e865ccb4eb8cad98ed397fc18ec63f99215a8d2
Joe Amburgey <jamburgey3@gatech.edu>
2021-10-19 19:10:38 -0400

    Initial local machine commit

---
+++ packages/time_value.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class Annuity:
+    def __init__():
+        pass
+
+
+    def present_value():
+        pass
+
+
+    def future_value():
+        pass
+
+
+class Hold:
+    def __init__():
+        pass
+
+
+    def present_value():
+        pass
+
+
+    def future_value():
+        pass
